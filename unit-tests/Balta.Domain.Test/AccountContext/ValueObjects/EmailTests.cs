d57021894362ddd1bf0e4c8cdcb17e6b422c0145
Guilherme Bley <guilhermecbley@gmail.com>
2024-10-25 18:52:10 -0300

    feat(EmailTests)

--- unit-tests/Balta.Domain.Test/AccountContext/ValueObjects/EmailTests.cs
+++ unit-tests/Balta.Domain.Test/AccountContext/ValueObjects/EmailTests.cs
@@ -15,26 +15,71 @@ public class EmailTests
     }
     
     [Fact]
-    public void ShouldTrimEmail() => Assert.Fail();
-    
+    public void ShouldTrimEmail()
+    {
+        const string expected = "myemail@gmail.com";
+
+        var result = Email.ShouldCreate(" " + expected + " ", FakeDateTimeProvider.Default);
+
+        Assert.Equal(expected, result.Address);
+    }
+
     [Fact]
-    public void ShouldFailIfEmailIsNull() => Assert.Fail();
-    
+    public void ShouldFailIfEmailIsNull()
+    {
+#pragma warning disable CS8625
+        var act = () 
+            => Email.ShouldCreate(null, FakeDateTimeProvider.Default);
+#pragma warning restore CS8625
+
+        Assert.ThrowsAny<Exception>(act);
+    }
+
     [Fact]
-    public void ShouldFailIfEmailIsEmpty() => Assert.Fail();
-    
+    public void ShouldFailIfEmailIsEmpty()
+    {
+        var act = ()
+            => Email.ShouldCreate(string.Empty, FakeDateTimeProvider.Default);
+
+        Assert.ThrowsAny<Exception>(act);
+    }
+
     [Fact]
-    public void ShouldFailIfEmailIsInvalid() => Assert.Fail();
-    
+    public void ShouldFailIfEmailIsInvalid()
+    {
+        const string invalidEmail = "invalidemail";
+
+        var act = ()
+            => Email.ShouldCreate(invalidEmail, FakeDateTimeProvider.Default);
+
+        Assert.ThrowsAny<Exception>(act);
+    }
+
     [Fact]
-    public void ShouldPassIfEmailIsValid() => Assert.Fail();
-    
+    public void ShouldPassIfEmailIsValid()
+    {
+        const string validEmail = "myemail@gmail.com";
+
+        var result = Email.ShouldCreate(validEmail, FakeDateTimeProvider.Default);
+
+        Assert.NotEmpty(result.Address);
+    }
+
     [Fact]
-    public void ShouldHashEmailAddress() => Assert.Fail();
-    
+    public void ShouldHashEmailAddress()
+    {
+        const string validEmail = "myemail@gmail.com";
+
+        var result = Email.ShouldCreate(validEmail, FakeDateTimeProvider.Default);
+
+        Assert.NotEmpty(result.Hash);
+    }
+
     [Fact]
-    public void ShouldExplicitConvertFromString() => Assert.Fail();
-    
+    public void ShouldExplicitConvertFromString()
+    {
+    }
+
     [Fact]
     public void ShouldExplicitConvertToString() => Assert.Fail();
     
